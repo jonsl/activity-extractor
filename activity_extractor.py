8f982059fb62ec3fee66cd10cf671f6f57bfb5ef
Jon Slater <jonathan.slater@crowdvision.co.uk>
2018-01-28 22:04:42 +0000

    fixed moving_time not displayed correctly; formatting; refactor

--- activity_extractor.py
+++ activity_extractor.py
@@ -1,5 +1,7 @@
 
 import xlsxwriter
+import datetime
+import time
 
 from stravalib.client import Client
 
@@ -19,7 +21,8 @@ client = Client(access_token=ACCESS_TOKEN)
 client.access_token = ACCESS_TOKEN;
 
 athlete = client.get_athlete()
-print("\n=> for athlete {id}, I now have an access token {token}\n".format(id=athlete.id, token=client.access_token))
+print("\n=> for athlete {id}, I now have an access token {token}\n".format(
+	id=athlete.id, token=client.access_token))
 
 # get club to work with
 clubs=[]
@@ -29,26 +32,38 @@ for idx, club in enumerate(client.get_athlete_clubs()):
 
 selected_club_idx = int(input('\nPlease enter club # > '))
 
-workbook = xlsxwriter.Workbook('club_data.xlsx', {'default_date_format':'dd/mm/yy'})
+now = datetime.datetime.now()
+filename = now.strftime('%Y-%m-%d_club_data.xlsx');
+
+workbook = xlsxwriter.Workbook(filename, {'default_date_format':'dd/mm/yy'})
 worksheet = workbook.add_worksheet()
 
-row=1
-col=1
+bold = workbook.add_format({'bold': True})
 
-worksheet.write(row, col, 'ID')
-worksheet.write(row, col+1, 'Name')
-worksheet.write(row, col+2, 'Athlete Name')
-worksheet.write(row, col+3, 'Date')
-worksheet.write(row, col+4, 'Type')
-worksheet.write(row, col+5, 'Distance m')
-worksheet.write(row, col+6, 'Moving Time')
-worksheet.write(row, col+7, 'Average Speed m/s')
-worksheet.write(row, col+8, 'Max Speed m/s')
-worksheet.write(row, col+9, 'Total Elevation Gain m')
+row=0
+col=0
 
-worksheet.set_column('B:B', 10)
-worksheet.set_column('C:C', 40)
-worksheet.set_column('D:D', 15)
+worksheet.write(row, col, 'ID', bold)
+worksheet.write(row, col+1, 'Name', bold)
+worksheet.write(row, col+2, 'Athlete', bold)
+worksheet.write(row, col+3, 'Date', bold)
+worksheet.write(row, col+4, 'Type', bold)
+worksheet.write(row, col+5, 'Distance (m)', bold)
+worksheet.write(row, col+6, 'Moving Time (s)', bold)
+worksheet.write(row, col+7, 'Average Speed (m/s)', bold)
+worksheet.write(row, col+8, 'Max Speed (m/s)', bold)
+worksheet.write(row, col+9, 'Total Elevation Gain (m)', bold)
+
+worksheet.set_column('A:A', 10)
+worksheet.set_column('B:B', 26)
+worksheet.set_column('C:C', 10)
+worksheet.set_column('D:D', 9)
+worksheet.set_column('E:E', 8)
+worksheet.set_column('F:F', 12)
+worksheet.set_column('G:G', 13)
+worksheet.set_column('H:H', 17)
+worksheet.set_column('I:I', 14)
+worksheet.set_column('J:J', 19)
 
 row += 1
 
@@ -64,19 +79,20 @@ for idx, club_activity in enumerate(client.get_club_activities(clubs[selected_cl
 	worksheet.write_datetime(row, col+3, club_activity.start_date.replace(tzinfo=None))
 	worksheet.write(row, col+4, club_activity.type)
 	worksheet.write(row, col+5, club_activity.distance)
-	worksheet.write(row, col+6, club_activity.moving_time)
+	worksheet.write(row, col+6, club_activity.moving_time.total_seconds())
 	worksheet.write(row, col+7, club_activity.average_speed)
 	worksheet.write(row, col+8, club_activity.max_speed)
 	worksheet.write(row, col+9, club_activity.total_elevation_gain)
 	row += 1
-	col = 1
+	col = 0
 	total_distance += int(club_activity.distance)
 
 workbook.close()
 
-print('\nwritten \'club_data.xlsx\'')
+print('\nwrote \'{0}\''.format(filename))
 
 print('\nprocessed %s activities' % idx)
 
-print('\nTotal activities distance is {0} km / {1} mi\n'.format(round(total_distance / 1000.0, 2), round((total_distance * 0.621371) / 1000.0, 2)))
+print('\nTotal activities distance is {0} km / {1} mi\n'.format(
+	round(total_distance / 1000.0, 2), round((total_distance * 0.621371) / 1000.0, 2)))
 
